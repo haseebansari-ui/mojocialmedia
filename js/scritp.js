9495a86b2e6262a3b8c38dd73121188574cc650e
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-06-17 18:36:03 +0530

    working on index page

--- js/scritp.js
+++ js/scritp.js
@@ -1,2 +1,16 @@
 "use strict"
 
+// scroll Header
+
+window.onscroll = function() {headerScroll()};
+
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
+
+function headerScroll() {
+  if (window.pageYOffset > 0) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
