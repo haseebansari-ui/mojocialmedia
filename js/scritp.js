9b6331603969f790405f9ac465b246e080542b87
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-06-19 13:37:40 +0530

    add media css, Working on responsive

--- js/scritp.js
+++ js/scritp.js
@@ -4,7 +4,7 @@
 
 window.onscroll = function() {headerScroll()};
 
-var header = document.querySelector("header");
+var header = document.querySelector(".header");
 var sticky = header.offsetTop;
 
 function headerScroll() {
@@ -14,3 +14,23 @@ function headerScroll() {
     header.classList.remove("active");
   }
 }
+
+
+// Menue
+
+const openMenuBtn = document.querySelector(".menu-open");
+const closeMenuBtn = document.querySelector(".menu-close");
+const opneMenue = document.querySelector("nav");
+
+if(openMenuBtn){
+  openMenuBtn.addEventListener("click", ()=>{
+    opneMenue.classList.add("active");
+  })
+}
+if(closeMenuBtn){
+  closeMenuBtn.addEventListener("click", ()=>{
+    opneMenue.classList.remove("active");
+  })
+  
+}
+
