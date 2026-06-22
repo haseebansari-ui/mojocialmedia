b110202d06ca7f859a466dca6c8115caf6840110
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-06-22 18:55:09 +0530

    changen in case study page

--- js/scritp.js
+++ js/scritp.js
@@ -98,3 +98,19 @@ items.forEach(item => {
                 hideExtraCards();
             }
         });
+
+
+// Casestudy
+
+const buttons = document.querySelectorAll(".filter-btn");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        buttons.forEach(item =>
+            item.classList.remove("active")
+        );
+
+        btn.classList.add("active");
+    });
+});
